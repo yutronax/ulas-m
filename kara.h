13de0322baed9534dbda5a5fa913e91c4d160896
yutronax <125690220+yutronax@users.noreply.github.com>
2024-03-04 13:11:39 +0300

    Update kara.h

--- kara.h
+++ kara.h
@@ -1,34 +1,34 @@
 #pragma once
 #pragma once
-#include "ulasım.h"
+#include "ulasÄ±m.h"
 
-class kara : public ulasım {
+class kara : public ulasÄ±m {
 public:
 	
 	void bilgi() {
 		
 	}
-	void gidisfiyatı(int _sehir2) {
-arac = 550;
+	void gidisfiyatÄ±(int _sehir2) {
+arac = 145;
 		
 		string metin;
 		ptr = &_sehir2;
 		metin = il[*ptr];
 		int fiyat = metin.length() * arac;
-		cout << "Bilet fiyatı: " << fiyat << " TL\n";
+		cout << "Bilet fiyatÄ±: " << fiyat << " TL\n";
 	}
-	void gidissüresi(int _sehir) {
-		süre = 45;
+	void gidissÃ¼resi(int _sehir) {
+		sÃ¼re = 45;
 		int saat = 0;
 		int dakika = 0;
 		int sure;
 		ptr = &_sehir;
 		string metin1;
 		metin1 = il[*ptr];
-		sure = metin1.length() * süre;
+		sure = metin1.length() * sÃ¼re;
 		saat = sure / 60;
 		dakika = sure % 60;
-		cout << "Gidiş süresi: " << saat << " saat " << dakika << " dakika\n";
+		cout << "GidiÅŸ sÃ¼resi: " << saat << " saat " << dakika << " dakika\n";
 	}
 private:
 	string ad;
