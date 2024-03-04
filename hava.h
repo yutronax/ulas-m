fe10f2702327cdaf0e34e482869c69699232ae2d
yutronax <125690220+yutronax@users.noreply.github.com>
2024-03-04 13:12:13 +0300

    Update hava.h

--- hava.h
+++ hava.h
@@ -1,35 +1,35 @@
 #pragma once
 #pragma once
-#include "ulasım.h"
+#include "ulasÄ±m.h"
 
-class hava : public ulasım {
+class hava : public ulasÄ±m {
 public:
 	
 	
 	void bilgi2() {
 		
 	}
-	void gidisfiyatı(int _sehir2) {
-arac = 1000;
+	void gidisfiyatÄ±(int _sehir2) {
+arac = 400;
 		
 		string metin;
 		ptr = &_sehir2;
 		metin = il[*ptr];
 		int fiyat = metin.length() * arac;
-		cout << "Bilet fiyatı: " << fiyat << " TL\n";
+		cout << "Bilet fiyatÄ±: " << fiyat << " TL\n";
 	}
-	void gidissüresi(int _sehir) {
-		süre = 15;
+	void gidissÃ¼resi(int _sehir) {
+		sÃ¼re = 15;
 		int saat = 0;
 		int dakika = 0;
 		int sure;
 		ptr = &_sehir;
 		string metin1;
 		metin1 = il[*ptr];
-		sure = metin1.length() * süre;
+		sure = metin1.length() * sÃ¼re;
 		saat = sure / 60;
 		dakika = sure % 60;
-		cout << "Gidiş süresi: " << saat << " saat " << dakika << " dakika\n";
+		cout << "GidiÅŸ sÃ¼resi: " << saat << " saat " << dakika << " dakika\n";
 	}
 
 private:
